423473f0cf53f1ca5d932e8efa2a435e6801cb59
Ekatya6 <ekaterinasoloveyeva@gmail.com>
2024-03-17 18:19:20 +0500

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте произвольный массив. Выведите его элементы, начиная с конца. Использовать рекурсию. 
+
+using System.Globalization;
+
+int[] numbers = { 3, 2, 7, 6, 5, 8 }; 
+
+void PrintElementsReverse(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        System.Console.WriteLine(array[index]);
+        PrintElementsReverse(array, index - 1);
+    }
+}
+
+PrintElementsReverse(numbers, numbers.Length - 1);
